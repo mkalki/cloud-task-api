a22ae00c7b5def072346bcfe2b67eb0b497f0a96
mkalki <121169609+mkalki@users.noreply.github.com>
2026-07-11 14:09:54 +0530

    Initial Spring Boot project with first REST endpoint

---
+++ src/main/java/com/mkalki/cloudtaskapi/CloudTaskApiApplication.java
@@ -0,0 +1,13 @@
+package com.mkalki.cloudtaskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTaskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudTaskApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mkalki/cloudtaskapi/CloudTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkalki.cloudtaskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTaskApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
