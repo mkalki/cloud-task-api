26eefeca58eff108f36487186a3afd957808b4ff
mkalki <121169609+mkalki@users.noreply.github.com>
2026-07-12 19:41:16 +0530

    Add Task model and GET /tasks endpoint

--- src/main/java/com/mkalki/cloudtaskapi/controller/HelloController.java
+++ src/main/java/com/mkalki/cloudtaskapi/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.mkalki.cloudtaskapi.controller;
 
+import com.mkalki.cloudtaskapi.model.Task;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.mkalki.cloudtaskapi.model.Greeting;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -11,5 +14,25 @@ public class HelloController {
     public Greeting hello(){
         Greeting greeting=new Greeting("hello world","mkalki");
         return greeting;
+    }
+
+    @GetMapping("/tasks")
+    public List<Task> getTasks() {
+        List<Task> tasks = new ArrayList<>();
+        Task task1 = new Task(1L, "Learn Spring Boot",
+                "Complete Lesson 3", false);
+
+        Task task2 = new Task(2L, "Learn collection frameworks",
+                "Complete Lesson 1", false);
+
+        Task task3 = new Task(3L, "solve leetcode",
+                "2 sum", false);
+
+        tasks.add(task1);
+        tasks.add(task2);
+        tasks.add(task3);
+
+        return tasks;
+
     }
 }
---
+++ src/main/java/com/mkalki/cloudtaskapi/model/Task.java
@@ -0,0 +1,36 @@
+package com.mkalki.cloudtaskapi.model;
+
+public class Task {
+
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+
+    public Task(Long id,
+                String title,
+                String description,
+                boolean completed){
+
+        this.id=id;
+        this.title=title;
+        this.description=description;
+        this.completed=completed;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public boolean isCompleted(){
+        return completed;
+    }
+}
