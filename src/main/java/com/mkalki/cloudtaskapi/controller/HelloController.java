e2ea7befb2ede220d93f7610b0c59ee5ca35c6eb
mkalki <121169609+mkalki@users.noreply.github.com>
2026-07-14 18:16:30 +0530

    feat: return 201 Created for POST /tasks

--- src/main/java/com/mkalki/cloudtaskapi/controller/HelloController.java
+++ src/main/java/com/mkalki/cloudtaskapi/controller/HelloController.java
@@ -2,10 +2,8 @@ package com.mkalki.cloudtaskapi.controller;
 
 import com.mkalki.cloudtaskapi.dto.CreateTaskRequest;
 import com.mkalki.cloudtaskapi.model.Task;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import com.mkalki.cloudtaskapi.model.Greeting;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +30,7 @@ public class HelloController {
     }
 
     @PostMapping("/tasks")
+    @ResponseStatus(HttpStatus.CREATED)
     public Task createTask(@RequestBody CreateTaskRequest request){
         return taskService.createTask(request);
     }
