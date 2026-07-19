b5030c567d621e441a44911ccf9e1229065a4137
mkalki <121169609+mkalki@users.noreply.github.com>
2026-07-19 20:23:53 +0530

    feat: add sorting support for paginated tasks

--- src/main/java/com/mkalki/cloudtaskapi/controller/TaskController.java
+++ src/main/java/com/mkalki/cloudtaskapi/controller/TaskController.java
@@ -6,6 +6,7 @@ import com.mkalki.cloudtaskapi.model.Task;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
@@ -30,9 +31,23 @@ public class TaskController {
     @GetMapping("/tasks")
     public Page<Task> getTasks(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id,asc") String sort
     ) {
-        Pageable pageable = PageRequest.of(page, size);
+
+        String[] sortParts = sort.split(",");
+
+        Sort sortObject;
+
+        if(sortParts.length == 1){
+            sortObject= Sort.by(sortParts[0]).ascending();
+        }
+        else if("desc".equalsIgnoreCase(sortParts[1])){
+            sortObject=Sort.by(sortParts[0]).descending();
+        }else {
+            sortObject=Sort.by(sortParts[0]).ascending();
+        }
+        Pageable pageable = PageRequest.of(page, size ,sortObject);
         return taskService.getAllTasks(pageable);
     }
 
